0a4ca232dfab11ba6d1462d74e5d8f6b65c93c48
Kelvin Keegan <kelvinkeegen17@gmail.com>
2022-03-31 11:57:28 +0300

    Added a common response thread

--- src/main/java/com/keeganapps/springsecurityapp/service/ClientDetailsService.java
+++ src/main/java/com/keeganapps/springsecurityapp/service/ClientDetailsService.java
@@ -1,21 +1,29 @@
 package com.keeganapps.springsecurityapp.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.keeganapps.springsecurityapp.entity.models.StatusResponseBody;
 import com.keeganapps.springsecurityapp.entity.tables.ClientUser;
 import com.keeganapps.springsecurityapp.repository.ClientUserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ClientDetailsService implements UserDetailsService {
 
     private final ClientUserRepository clientUserRepository;
+    private final HttpServletResponse httpServletResponse;
 
     // Spring security util class for user validation
 
@@ -26,14 +34,25 @@ public class ClientDetailsService implements UserDetailsService {
 
         if (optionalClientUser.isEmpty()){
 
-         throw new UsernameNotFoundException("User Not found with the username provided");
+            ObjectMapper objectMapper = new ObjectMapper();
 
+         httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+         httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            try {
+                objectMapper.writeValue(httpServletResponse.getOutputStream(),new StatusResponseBody(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,null,"User Not found with the username provided"));
+                log.error("User Not found with the username provided");
+
+            } catch (IOException e) {
+
+                log.error(e.getMessage());
+            }
         }
 
-        return new User(
-                optionalClientUser.get().getUsername(),
-                optionalClientUser.get().getPassword(),
-                optionalClientUser.get().getAuthorities()
-        );
+            return new User(
+                    optionalClientUser.get().getUsername(),
+                    optionalClientUser.get().getPassword(),
+                    optionalClientUser.get().getAuthorities()
+            );
+
     }
 }
